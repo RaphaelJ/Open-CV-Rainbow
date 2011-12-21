148aa50a62cc1aa87357143db76b1c9687968959
Raphael Javaux <raphaeljavaux@gmail.com>
2011-12-21 20:21:07 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+
+# Raphael Javaux <raphaeljavaux@gmail.com> - December 2011
+
+import sys, cv
+from PyQt4 import QtGui
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+
+    w = QtGui.QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('Simple')
+    w.show()
+    
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
